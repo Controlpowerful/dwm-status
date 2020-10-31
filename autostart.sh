f27e7bdb72355b75ff482fe7b979358f3a07e02c
Controlpowerful <2095655680@qq.com>
2020-10-31 16:11:00 +0800

    change autostart

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,7 @@
 /bin/bash ~/.dwm/dwm-status-refresh.sh &
 /bin/bash ~/.dwm/dwm-status.sh & 
 picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-#picom -b
-fcitx
-albert &
+# picom -b
+fcitx5 &
+# albert &
 feh --bg-scale ~/Downloads/1098407.jpg
